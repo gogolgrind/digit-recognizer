c3d4b99a5359d855f32dbe88ceee174910a3b884
Konstantin <gogolgrind@yandex.ru>
2015-12-28 03:02:17 +0300

    Update digit-recognizer.py
    adds svm and CNN algorithms

--- digit-recognizer.py
+++ digit-recognizer.py
@@ -5,76 +5,150 @@ Created on Wed Dec 23 22:03:30 2015
 based on : http://lenguyenthedat.com/minimal-data-science-3-mnist-neuralnet/
            https://github.com/lenguyenthedat/kaggle-for-fun/blob/master/digit-recognizer/digit-recognizer.py
 """
+from __future__ import print_function
 import pandas as pd
 import scipy as sp
 import csv
 from time import time
-from os import path,makedirs
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
-#from sknn import mlp
+import sklearn.metrics
+from os import path,makedirs,name as os_name
+from sklearn.ensemble import RandomForestClassifier  
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler,RobustScaler
+from sknn import mlp
+from datetime import datetime
+
+# disable warnings 
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
 def main():
     ok = 'ok'
     # params
     pd.options.mode.chained_assignment = None
-    sample = 0
+    sample = 1
+    scale = 0
+    is_win = os_name == 'nt'
+    is_pca = 0
     target = 'Label' if sample else 'label'
     # reading data
-    base_path = 'C:\\Users\\Gogol\\Desktop\\kaggle\\Digit Recognizer\\data'
+    base_path = ''
+    if is_win:
+        base_path = 'C:\\Users\\Gogol\\Desktop\\kaggle\\Digit Recognizer\\data'
+    else:
+        base_path = '/home/cvlab/Рабочий стол/kaggle/Digit Recognizer/data'
+    print('Data reading starts')
     if not sample:
         train = pd.read_csv(path.join(base_path,'train.csv'))
         test = pd.read_csv(path.join(base_path,'test.csv'))
     else:
-        data = pd.read_csv(path.join(base_path,'train-200.csv'))
-        # show digits first 10
-        i = 0
-        for line in open(path.join(base_path,'train-200.csv')):
-            # ignore laberl
-            if i > 0 and i < 10:
-                im = sp.array([int(elem) for elem in line.strip().split(',')])[:-1].reshape((28,28))
-                plt.imshow(im)
-                plt.gray()
-                plt.show()
-            i += 1
+        data = pd.read_csv(path.join(base_path,'train-1000.csv'))
         # split data into test and train data 25 % and 75 %
+    if sample:
         data['is_train'] = sp.random.uniform(0, 1, len(data)) <= 0.75
         train = data[data['is_train'] == True]
         test  = data[data['is_train'] == False]
+    print('Data reading ends')
     # get_features name
     features = test.columns.tolist()
-    classifiers = []
+
     if sample:
         features.remove('is_train')
-        features.remove('Label')
+        features.remove(target)
+        pass
+    # scaling. Dont use it with SVM
+    if scale:
+        print('Data scaling starts')
+        # StandardScaler get warnings. Why ?
+        scaler = RobustScaler()
+        for col in features:
+            scaler.fit(list(train[col])+list(test[col]))
+            train[col] = scaler.transform(train[col])
+            test[col] = scaler.transform(test[col])
+        print('Data scaling ends\n \n')
     # Create array of classifiers
     classifiers = []
     classifiers.append(RandomForestClassifier(n_estimators=256, max_depth=64))
+    classifiers.append(SVC(kernel ='rbf',C = 2))
+#    classifiers.append(mlp.Classifier(
+#            layers=[
+#                mlp.Layer('Rectifier', units=100),
+#                mlp.Layer("Tanh", units=100),
+#                mlp.Layer("Sigmoid", units=100),
+#                mlp.Layer('Softmax')],
+#            learning_rate=0.001,
+#            learning_rule='momentum',
+#            learning_momentum=0.9,
+#            batch_size=100,
+#            valid_size=0.01,
+#            n_stable=20,
+#            n_iter=200,
+#            verbose=True))
+
+#    classifiers.append(mlp.Classifier(
+#                layers=[
+#                    mlp.Convolution("Rectifier", channels=16, kernel_shape=(5,5)),
+#                    mlp.Convolution("Rectifier", channels=8, kernel_shape=(2,2)),
+#                    mlp.Convolution("Rectifier", channels=8, kernel_shape=(2,2)),
+#                    mlp.Layer('Rectifier', units=100),
+#                    mlp.Layer('Softmax')],
+#                learning_rate=0.001,
+#                learning_rule='momentum',
+#                learning_momentum=0.9,
+#                batch_size=100,
+#                valid_size=0.01,
+#                n_stable=20,
+#                n_iter=50,
+#                verbose=True))
+            
+   
     # Train
+    cnt = 0
+    pca = PCA(n_components=35,whiten=True)
     for classifier in classifiers:
+        class_name = classifier.__class__.__name__
+        print('Training with %s  starts' % class_name)
         st = time()
-        classifier.fit(sp.array(train[list(features)]),train[target])
+        labels = train[target]
+        train_data = sp.array(train[list(features)])
+        if class_name == 'SVC' or is_pca:
+            train_data = pca.fit_transform(train_data)
+        classifier.fit(train_data,labels)
         fn = time()
-        print classifier.__class__.__name__,'with training time:',fn-st
-    if sample:
-        a_val =  accuracy_score(test[target].values,classifier.predict(sp.array(test[features])))
-        print classifier.__class__.__name__,'Accuracy Score:',a_val
-    else:
-        cnt = 0
-        for classifier in classifiers:
-            cnt += 1
-            res_path = 'C:\\Users\\Gogol\\Desktop\\kaggle\\Digit Recognizer\\result'
+        print (class_name,'with training time:',fn-st)
+        print('Training with %s ends' % class_name)
+        print('Eval with %s starts' % class_name)
+        a_val = 0
+        test_data = sp.array(test[features])
+        if class_name == 'SVC' or is_pca:
+            test_data = pca.transform(test_data)
+        pred  = classifier.predict(test_data)
+        if sample:
+            a_val =  sklearn.metrics.accuracy_score(test[target].values,pred)
+            print (classifier.__class__.__name__,'Accuracy Score:',a_val)
+        else:
+            res_path = ''
+            if is_win:
+                res_path = 'C:\\Users\\Gogol\\Desktop\\kaggle\\Digit Recognizer\\result'
+            else:
+                res_path = '/home/cvlab/Рабочий стол/kaggle/Digit Recognizer/result'
             if not path.exists(res_path):
                 makedirs(res_path)
-            csvfile = path.join(res_path,classifier.__class__.__name__ + '-'+ str(cnt) + '-submit.csv')
-            print(csvfile)
-            pred  = classifier.predict(sp.array(test[features]))
+            classifier_name = classifier.__class__.__name__
+            if classifier_name == 'classifier':
+                classifier_name = 'DNN_'+ classifier_name
+            classifier_name = str(cnt) + '_' + classifier_name + '_' +  datetime.now().strftime("%Y-%m-%d_%H-%M")
+            csvfile = path.join(res_path,classifier_name + '_' + 'submit.csv')
             with open(csvfile,'w') as out:
                 writer = csv.writer(out,lineterminator='\n')
                 writer.writerow(['ImageId',target])
                 m = len(pred)
                 for i in range(m):
-                    writer.writerow([i+1,pred[i]])
+                    pred_val = pred[i] if type(pred[i]) != sp.ndarray else pred[i][0]
+                    writer.writerow([i+1,pred_val])
+        print('Eval ends\n')
+        cnt += 1
 if __name__ == '__main__':
     main()
-
